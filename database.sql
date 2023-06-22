349321bcddbf72e2bc2ae268b2c82db1f650fd94
Erich Engler <erich.engler@hotmail.com>
2023-06-21 19:35:23 -0500

    set up main - login and registration working

--- database.sql
+++ database.sql
@@ -3,8 +3,69 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- Create the ‘user’ table
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+ id SERIAL PRIMARY KEY,
+ first_name VARCHAR(50),
+ last_name VARCHAR(50),
+ birthdate DATE,
+ email VARCHAR(100),
+ password varchar(250),
+ username varchar(50)
 );
+
+ -- Create the ‘journal’ table
+CREATE TABLE journal (
+ id SERIAL PRIMARY KEY,
+ user_id INT REFERENCES "user"(id),
+ journal VARCHAR(5000) not null,
+ entry_date DATE not null,
+ mood int null
+);
+
+-- Create the ‘content’ table
+CREATE TABLE content (
+ id SERIAL PRIMARY KEY,
+ file_name VARCHAR(250) UNIQUE,
+ s3_key VARCHAR(250) UNIQUE,
+ created_by INT REFERENCES "user"(id)
+);
+
+-- Create the ‘content_seen’ table
+CREATE TABLE content_seen (
+ id SERIAL PRIMARY KEY,
+ content_id INT REFERENCES content(id),
+ consumer_id INT REFERENCES "user"(id),
+ favorite INT DEFAULT 0
+);
+
+-- Create the ‘fin_goal’ table
+--CREATE TABLE fin_goal (
+-- id SERIAL PRIMARY KEY,
+-- income_yr DOUBLE PRECISION,
+-- monthly_goal DOUBLE PRECISION,
+-- monthly_spending DOUBLE PRECISION,
+-- user_id INT REFERENCES “user”(id)
+--);
+
+-- Create the ‘badges’ table
+CREATE TABLE badges (
+ id SERIAL PRIMARY KEY,
+ badge_name VARCHAR(50) UNIQUE,
+ summary VARCHAR(1000)
+);
+
+-- Create the ‘badge_earned’ table
+CREATE TABLE badge_earned (
+ id SERIAL PRIMARY KEY,
+ badge_id INT REFERENCES badges(id),
+ user_id INT REFERENCES "user"(id)
+);
+
+ select * from "user" ;
+ select * from journal ;
+ select * from "content" ;
+ select * from content_seen ;
+ select * from badges ;
+ select * from badge_earned ;
+ -- insert statments
