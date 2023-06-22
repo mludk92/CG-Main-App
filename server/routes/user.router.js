23795913f05e3b8bbaf456eb6ec797bb0d847af7
Erich Engler <erich.engler@hotmail.com>
2023-06-21 20:00:15 -0500

    finished main template - registration and login working

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -20,11 +20,16 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const birthdate = req.body.birthdate;
+  const email = req.body.email;
+  const pronoun = req.body.pronoun;
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "user" (username, password, first_name, last_name, birthdate, email, pronoun)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, firstName, lastName, birthdate, email, pronoun])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
---
+++ src/templateComponents/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function RegisterForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [email, setEmail] = useState('');
+  const [pronoun, setPronoun] = useState('');
+
+  const errors = useSelector((store) => store.errors);
+  const dispatch = useDispatch();
+
+  const registerUser = (event) => {
+    event.preventDefault();
+
+    dispatch({
+      type: 'REGISTER',
+      payload: {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        birthdate: birthdate,
+        email: email,
+        pronoun: pronoun,
+      },
+    });
+  }; // end registerUser
+
+  return (
+    <form className="formPanel" onSubmit={registerUser}>
+      <h2>Register User</h2>
+      {errors.registrationMessage && (
+        <h3 className="alert" role="alert">
+          {errors.registrationMessage}
+        </h3>
+      )}
+      <div>
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={username}
+            required
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            required
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="firstName">
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={firstName}
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="lastName">
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={lastName}
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="pronoun">
+          Preferred Pronoun:
+          <input
+            type="text"
+            name="pronoun"
+            value={pronoun}
+            required
+            onChange={(event) => setPronoun(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="birthdate">
+          Birthdate:
+          <input
+            type="date"
+            name="birthdate"
+            value={birthdate}
+            required
+            onChange={(event) => setBirthdate(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={email}
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <input className="btn" type="submit" name="submit" value="Register" />
+      </div>
+    </form>
+  );
+}
+
+export default RegisterForm;
