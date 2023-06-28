065eae6200a664396af6a676560f9e47f810b2fd
Mitch Ludkey <mludk92@hotmail.com>
2023-06-28 18:03:58 -0500

    journal nav and links are set up as well as base component

---
+++ src/newComponents/Journals/Journals.jsx
@@ -0,0 +1,13 @@
+function Journals() {
+
+    return (
+        <div className="container">
+            <h3>Welcome to the Journals Page</h3>
+            <p>Map of journals goes here </p>
+            <input type="text"></input>
+            <button>Submit</button>
+        </div>
+    );
+};
+
+export default Journals;
