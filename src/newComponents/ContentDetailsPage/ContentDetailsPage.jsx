346816831248ccaa5e48882576fbcb1e48d2e92b
Erich Engler <erich.engler@hotmail.com>
2023-06-28 18:11:11 -0500

    created media player on content details page using m4a file and image from public folder

--- src/newComponents/ContentDetailsPage/ContentDetailsPage.jsx
+++ src/newComponents/ContentDetailsPage/ContentDetailsPage.jsx
@@ -1,9 +1,18 @@
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
 function ContentDetails() {
 
     return (
         <div className="container">
-            <h3>Welcome to the Content Details Page</h3>
-            <p>Media player goes here</p>
+            
+            <center>
+                <h3>Content Details</h3>
+                <img src="images/rocks.png" style={{border: '1px solid black'}} />
+                <br /><br />
+                <AudioPlayer src="audio/Financial-wellbeing-meditation.m4a"/>
+            </center>
+
         </div>
     );
 }
