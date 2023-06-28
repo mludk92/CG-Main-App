8ef7cfee742c01b3ba0bb5566a45384018609ca9
Erich Engler <erich.engler@hotmail.com>
2023-06-27 20:00:48 -0500

    created content details page component where media player will go

---
+++ src/newComponents/ContentDetailsPage/ContentDetailsPage.jsx
@@ -0,0 +1,11 @@
+function ContentDetails() {
+
+    return (
+        <div className="container">
+            <h3>Welcome to the Content Details Page</h3>
+            <p>Media player goes here</p>
+        </div>
+    );
+}
+
+export default ContentDetails;
