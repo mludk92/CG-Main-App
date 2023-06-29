445bb8d1bf978d26a0df933ca2dc7055f2adf051
Erich Engler <erich.engler@hotmail.com>
2023-06-28 21:02:08 -0500

    finished basic styling for journals

--- src/newComponents/Journals/Journals.jsx
+++ src/newComponents/Journals/Journals.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Card, CardContent, Typography, IconButton, Box } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Journals() {
   const dispatch = useDispatch();
@@ -9,13 +12,68 @@ function Journals() {
     dispatch({ type: "FETCH_JOURNAL" });
   }, [dispatch]);
 
+  // Function to format the date
+  const formatDate = (dateString) => {
+    const options = { month: "2-digit", day: "2-digit", year: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  }
+
   return (
     <div className="container">
-      <h3>Welcome to the Journals Page</h3>
-      <p>Map of journals goes here</p>
-      <pre>{JSON.stringify(journalEntries)}</pre>
-      <input type="text" />
-      <button>Submit</button>
+      <center>
+        <Typography variant="h5">Journal</Typography>
+        <br />
+        <textarea
+          placeholder='Create a journal entry'
+          style={{ 
+            width: '90%', 
+            height: '100px', 
+            resize: 'none', 
+          }}
+        />
+        <br />
+        <button>Submit</button>
+      </center>
+
+      <br /><br />
+
+      <div className="journal-cards">
+
+        {/* Mapping journalEntries to be displayed on cards */}
+        {journalEntries.map((entry, index) => (
+          <Card key={index} variant="outlined" style={{ marginBottom: '10px' }}>
+            <CardContent>
+
+              <Box display="flex" justifyContent="space-between" alignItems="center" mb={1}>
+                
+                {/* Date and Mood from entry */}
+                <Typography variant="subtitle2">
+                  <span>Date: {formatDate(entry.entry_date)}</span> - <span>Mood: {entry.mood}</span>
+                </Typography>
+
+                <div>
+                  {/* Edit journal entry icon */}
+                  <IconButton aria-label="Edit">
+                    <EditIcon />
+                  </IconButton>
+
+                  {/* Delete journal entry icon */}
+                  <IconButton aria-label="Delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </div>
+              </Box>
+
+              {/* Journal entry text */}
+              <Typography variant="body1">
+                {entry.journal}
+              </Typography>
+
+            </CardContent>
+          </Card>
+        ))}
+
+      </div>
     </div>
   );
 }
