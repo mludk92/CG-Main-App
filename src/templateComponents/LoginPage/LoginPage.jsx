114b05864c8c6e61d94320bd05e41d690c56e7ba
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-06-29 21:59:30 -0400

    Did the MUI for login page and added a logo

--- src/templateComponents/LoginPage/LoginPage.jsx
+++ src/templateComponents/LoginPage/LoginPage.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import LoginForm from '../LoginForm/LoginForm';
-import { useHistory } from 'react-router-dom';
-
+import React from "react";
+import LoginForm from "../LoginForm/LoginForm";
+import { useHistory } from "react-router-dom";
+import { Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div>
+    <div style={{ height: "100vh", overflowY: "auto" }}>
+      <Paper
+        sx={{
+          borderRadius: "0 0 0 2rem",
+          minWidth: "100%",
+          minHeight: 85,
+          position: "absolute",
+          backgroundColor: "#3d71b8",
+          top: -30,
+        }}
+      />
+
+      <Typography
+        textAlign="center"
+        variant="h3"
+        style={{ color: "#3d71b8"}}
+      >
+        Welcome!
+      </Typography>
+      <br />
+      <center>
+        <img
+          src="Logo/changegrowerlogo.png"
+          alt="ChangeGrower Logo"
+          style={{ width: "200px", height: "150px" }}
+        />
+      </center>
       <LoginForm />
 
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
+        <h5>
+          Don't have an account?{" "}
+          <button
+            type="button"
+            className="btn btn_asLink"
+            onClick={() => {
+              history.push("/registration");
+            }}
+          >
+            Signup
+          </button>
+        </h5>
       </center>
     </div>
   );
