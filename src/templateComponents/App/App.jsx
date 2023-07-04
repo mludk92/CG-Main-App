22d3bad6e58787814766a71fe782034daa55698f
Kaelan Leahy <kaelan@leahyonline.com>
2023-07-03 21:00:05 -0500

    Made bar with icons on bottom of the screen. Only the logged in version.

--- src/templateComponents/App/App.jsx
+++ src/templateComponents/App/App.jsx
@@ -38,7 +38,6 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
         <Switch>
           {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
           <Redirect exact from="/" to="/home" />
@@ -156,6 +155,7 @@ function App() {
             <h1>404</h1>
           </Route>
         </Switch>
+        <Nav />
       </div>
     </Router>
   );
--- src/templateComponents/Nav/Nav.jsx
+++ src/templateComponents/Nav/Nav.jsx
@@ -1,47 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { useSelector } from 'react-redux';
 
 function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
-    <div className="nav">
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/home">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/explore">
-              Explore
-            </Link>
-
-            <Link className="navLink" to="/user">
-              User
-            </Link>
-
-            {/* <LogOutButton className="navLink" /> */}
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
+    <Box>
+      {/* If no user is logged in, show these links */}
+      {!user.id && (
+        // If there's no user, show login/registration links
+        <Link className="navLink" to="/login">
+          Login / Register
         </Link>
-      </div>
-    </div>
+      )}
+
+      {/* If a user is logged in, show these links */}
+      {user.id && (
+        <Paper elevation={3} sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+          <BottomNavigation>
+            <BottomNavigationAction 
+              component={Link}
+              to='/home'
+              icon={<HomeIcon />}
+            />
+            <BottomNavigationAction 
+              component={Link}
+              to='/explore'
+              icon={<ExploreIcon />}
+            />
+            <BottomNavigationAction 
+              component={Link}
+              to='/user'
+              icon={<PersonIcon />}
+            />
+            <BottomNavigationAction 
+              component={Link}
+              to='/settings'
+              icon={<SettingsIcon />}
+            />
+          </BottomNavigation>
+        </Paper>
+      )}
+    </Box>
   );
 }
 
