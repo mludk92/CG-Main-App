50af35ac6d6883018553f740a6963d72dcf518c2
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-07-04 15:32:56 -0400

    MUI and got journals and badges to display

--- src/templateComponents/LoginPage/LoginPage.jsx
+++ src/templateComponents/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import LoginForm from "../LoginForm/LoginForm";
 import LoginPageHeader from "./LoginPageHeader";
 import LoginPageFooter from "./LoginPageFooter";
 
+
 function LoginPage() {
   return (
     <div style={{height: "100vh", overflowY: "auto" }}>
