eca038939ed24a11ccaf910d478d9f6e678a09d9
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-07-07 15:53:33 -0400

    Journal Modal pops when clicking entries. limited 7 journal entries listing the latest at the top.

--- src/templateComponents/UserPage/UserPage.jsx
+++ src/templateComponents/UserPage/UserPage.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Divider } from "@mui/material";
 import Paper from "@mui/material/Paper";
@@ -9,6 +8,9 @@ import EmojiEventsTwoToneIcon from "@mui/icons-material/EmojiEventsTwoTone";
 import Card from "@mui/material/Card";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import {
+  Modal,
+  Typography,
+  CardContent,
   Table,
   TableBody,
   TableCell,
@@ -18,7 +20,10 @@ import {
   Button,
 } from "@mui/material";
 import "./UserPage.css";
-
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
@@ -26,6 +31,8 @@ function UserPage() {
   const journalEntries = useSelector((store) => store.journal);
   const dispatch = useDispatch();
   const history = useHistory();
+
+ 
 
   // Filter the badges earned by the logged-in user --Mitch
   console.log(badges)
@@ -38,14 +45,72 @@ function UserPage() {
     dispatch({ type: "FETCH_JOURNAL" });
   }, [dispatch]);
 
+  //sets latest badge from the array 
   const latestBadge = badges[badges.length - 1];
   console.log(latestBadge)
+  // console.log(latestBadge.badge_id)
+ 
 
+  const getBadgeImage = (badgeName) => {
+    switch (badgeName) {
+      case "First Meditation":
+        return "badges/FirstMeditation.png";   
+      case "On Fire":
+        return "badges/OnFire.png";
+      case "Reflection Time":
+        return "badges/ReflectionTime.png";
+      case "Meditation Scholar": 
+        return "badges/MeditationScholar.png"
+      default:
+        return "";
+    }
+  };
+  const latestBadgeImage = latestBadge ? getBadgeImage(latestBadge.badge_name) : "";
+ //latestBadge.Id
 
+  // Function to format the date
   const formatDate = (dateString) => {
     const options = { month: "2-digit", day: "2-digit", year: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
-  };
+  }
+
+  // Get current date
+  const currentDate = formatDate(new Date().toISOString());
+
+    // State for the modal
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [editedEntry, setEditedEntry] = useState({ journal: '', mood: '', id: '' });
+    // Storing new journal entry and mood in useState
+    const [newEntry, setEntry] = useState({ journal: '', mood: 1, date: currentDate });
+  
+    // Function to open the modal and set the edited entry
+    const handleOpenModal = (entry, mood, id) => {
+      setEditedEntry({ journal: entry, mood: mood, id: id });
+      setModalOpen(true);
+    };
+  
+    // Function to close the modal
+    const handleCloseModal = () => {
+      setModalOpen(false);
+    };
+  
+    // Function to handle the submit of the edited entry
+    const handleEditSubmit = () => {
+      dispatch({ type: 'EDIT_JOURNAL_ENTRY', payload: editedEntry });
+  
+      // Close the modal
+      handleCloseModal();
+    };
+  
+    // Handle change for journal entry
+    const handleEntryChange = (event) => {
+      setEntry({ ...newEntry, journal: event.target.value });
+    }
+  
+    // Handle change for mood
+    const handleMoodChange = (event) => {
+      setEntry({ ...newEntry, mood: event.target.value });
+    }
 
   return (
 
@@ -97,11 +162,17 @@ function UserPage() {
                 padding: "10px",
               }}
             >
-              <EmojiEventsTwoToneIcon
+               <img
+                src={latestBadgeImage}
+                alt="Latest Badge"
+                style={{ width: "50px",
+                height: "50px", }}
+              />
+              {/* <EmojiEventsTwoToneIcon
                 style={{ boxShadow: "0px 0px 25px rgba(131, 197, 95, 0.5)" }}
                 fontSize="small"
                 className="event-icon"
-              />
+              /> */}
               <div
                 style={{
                   display: "flex",
@@ -153,8 +224,8 @@ function UserPage() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {journalEntries.map((entry, index) => (
-              <TableRow key={index}>
+            {journalEntries.sort((a, b) => new Date(b.entry_date) - new Date(a.entry_date)).slice(0, 7).map((entry, index) => (
+              <TableRow  onClick={() => handleOpenModal(entry.journal, entry.mood, entry.id)} key={index}>
                 <TableCell>{formatDate(entry.entry_date)}</TableCell>
                 <TableCell>{entry.journal}</TableCell>
                 <TableCell className={`mood-cell`}>
@@ -169,6 +240,67 @@ function UserPage() {
 
       <LogOutButton className="btn" />
       <p>Your ID is {user.id}</p>
+
+
+      {/* Modal for editing journal entry */}
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <div className="modal-container">
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h6" style={{ textAlign: 'center' }}>
+                Edit Journal Entry
+              </Typography>
+              <TextField
+                multiline
+                rows={10}
+                value={editedEntry.journal}
+                onChange={(event) => setEditedEntry({ ...editedEntry, journal: event.target.value })}
+                fullWidth
+                variant="outlined"
+              />
+              <br /><br />
+              <div className="mood-select-container">
+                <div className="mood-select-wrapper">
+                  <InputLabel className="mood-label">Mood:</InputLabel>
+                  <Select
+                    value={editedEntry.mood}
+                    onChange={(event) => setEditedEntry({ ...editedEntry, mood: event.target.value })}
+                    className="mood-select-input"
+                    MenuProps={{
+                      anchorOrigin: {
+                        vertical: "bottom",
+                        horizontal: "left"
+                      },
+                      transformOrigin: {
+                        vertical: "top",
+                        horizontal: "left"
+                      }
+                    }}
+                  >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Select>
+                </div>
+                <button
+                  className="submit-button"
+                  type="submit"
+                  onClick={handleEditSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+
+
+
+
+            </CardContent>
+          </Card>
+        </div>
+      </Modal>
+
     </div>
   );
 }
