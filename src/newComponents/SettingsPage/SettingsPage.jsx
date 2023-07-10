7b59ae0971354d9b287427a6592b99a1364cbd18
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-07-09 21:59:16 -0400

    made a button to nav to upload

--- src/newComponents/SettingsPage/SettingsPage.jsx
+++ src/newComponents/SettingsPage/SettingsPage.jsx
@@ -1,11 +1,13 @@
 import Paper from "@mui/material/Paper";
 import { Typography, Divider } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router-dom";
+function SettingsPage() {
+    const history = useHistory();
 
-function SettingsPage () {
-
-    return (
-        <div className="container">
-          <Paper
+  return (
+    <div className="container">
+      <Paper
         sx={{
           minWidth: "100%",
           minHeight: 85,
@@ -15,18 +17,36 @@ function SettingsPage () {
           right: 0,
         }}
       />
-      <div style={{ display: "flex", alignItems: "center", marginTop: 40, position: "relative" }}>
-  <Typography variant="h4">Settings</Typography>
-  <img
-    src="Logo/cg-smile-icon.png"
-    alt="ChangeGrower Logo"
-    style={{ width: "60px", height: "50px", position: "absolute", right: 0, top: 0 }}
-  />
-</div>
-     
-      <Divider sx={{ marginTop: "10px", borderTopWidth: "4px", marginBottom: "20px"}} />
-        </div>
-    );
-};
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 40,
+          position: "relative",
+        }}
+      >
+        <Typography variant="h4">Settings</Typography>
+        <img
+          src="Logo/cg-smile-icon.png"
+          alt="ChangeGrower Logo"
+          style={{
+            width: "60px",
+            height: "50px",
+            position: "absolute",
+            right: 0,
+            top: 0,
+          }}
+        />
+      </div>
+
+      <Divider
+        sx={{ marginTop: "10px", borderTopWidth: "4px", marginBottom: "20px" }}
+      />
+      <Button onClick={() => {
+            history.push("/fileuploads");
+          }} variant="contained" style={{ marginTop: "10px", marginBottom: "5px", color: "#ffffff", backgroundColor: "#3d71b8" }}>Upload Content</Button>
+    </div>
+  );
+}
 
 export default SettingsPage;
