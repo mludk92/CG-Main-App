353f89e535af7bb9879b9fcca3d703c489696f23
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-07-09 22:48:21 -0400

    added links to about page and logout

--- src/newComponents/SettingsPage/SettingsPage.jsx
+++ src/newComponents/SettingsPage/SettingsPage.jsx
@@ -1,9 +1,20 @@
 import Paper from "@mui/material/Paper";
-import { Typography, Divider } from "@mui/material";
+import { Typography, Divider, List, ListItem, ListItemText, ListItemIcon} from "@mui/material";
 import Button from "@mui/material/Button";
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useHistory } from "react-router-dom";
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InfoIcon from '@mui/icons-material/Info';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { useDispatch } from 'react-redux';
 function SettingsPage() {
     const history = useHistory();
+    const dispatch = useDispatch();
+
+    
 
   return (
     <div className="container">
@@ -42,9 +53,57 @@ function SettingsPage() {
       <Divider
         sx={{ marginTop: "10px", borderTopWidth: "4px", marginBottom: "20px" }}
       />
-      <Button onClick={() => {
+     <List>
+     <ListItem button onClick={() => {
             history.push("/fileuploads");
-          }} variant="contained" style={{ marginTop: "10px", marginBottom: "5px", color: "#ffffff", backgroundColor: "#3d71b8" }}>Upload Content</Button>
+          }} >
+            <ListItemIcon>
+            <UploadFileIcon />
+            </ListItemIcon>
+            <ListItemText primary="Upload Content" />
+        </ListItem>
+        <Divider/>
+        <ListItem button>
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Account" />
+        </ListItem>
+        <Divider/>
+        <ListItem button>
+          <ListItemIcon>
+            <NotificationsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Notifications" />
+        </ListItem>
+        <Divider/>
+        <ListItem button>
+          <ListItemIcon>
+            <ContactSupportIcon />
+          </ListItemIcon>
+          <ListItemText primary="Help and Support" />
+        </ListItem>
+        <Divider/>
+        <ListItem button onClick={() => {
+            history.push("/about");
+          }} >
+          <ListItemIcon>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary="About" />
+        </ListItem>
+        <Divider/>
+        <ListItem button onClick={() => dispatch({ type: 'LOGOUT' })}>
+            <ListItemIcon>
+            <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary="Logout" />
+        </ListItem>
+        <Divider/>
+      </List>
+      {/* <Button onClick={() => {
+            history.push("/fileuploads");
+          }} variant="contained" style={{ marginTop: "10px", marginBottom: "5px", color:  "#3d71b8", backgroundColor: "#83C55F" }}>Upload Content</Button> */}
     </div>
   );
 }
