653b8226ebbf9479c0e884f915633e17eed30057
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-07-09 21:46:03 -0400

    added banner and title to settings page and fixed image tag on userpage

--- src/newComponents/SettingsPage/SettingsPage.jsx
+++ src/newComponents/SettingsPage/SettingsPage.jsx
@@ -1,9 +1,31 @@
+import Paper from "@mui/material/Paper";
+import { Typography, Divider } from "@mui/material";
+
 function SettingsPage () {
 
     return (
-        <>
-        <h3>Settings Page</h3>
-        </>
+        <div className="container">
+          <Paper
+        sx={{
+          minWidth: "100%",
+          minHeight: 85,
+          position: "absolute",
+          backgroundColor: "#3d71b8",
+          top: -30,
+          right: 0,
+        }}
+      />
+      <div style={{ display: "flex", alignItems: "center", marginTop: 40, position: "relative" }}>
+  <Typography variant="h4">Settings</Typography>
+  <img
+    src="Logo/cg-smile-icon.png"
+    alt="ChangeGrower Logo"
+    style={{ width: "60px", height: "50px", position: "absolute", right: 0, top: 0 }}
+  />
+</div>
+     
+      <Divider sx={{ marginTop: "10px", borderTopWidth: "4px", marginBottom: "20px"}} />
+        </div>
     );
 };
 
