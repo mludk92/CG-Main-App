094deecb94f4a4c7ffadad36eb81b18ac3f27dff
Mitchell Ludkey <mludk92@hotmail.com>
2023-07-13 07:03:16 -0500

    merging main

--- src/newComponents/HomePage/HomeHeader.jsx
+++ src/newComponents/HomePage/HomeHeader.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
@@ -8,22 +8,26 @@ function HomeHeader() {
     const [quote, setQuote] = useState('');
 
     useEffect(() => {
-        fetch('https://quote-garden.onrender.com/api/v3/quotes?genre=motivational')
-            .then(response => response.json())
-            .then(data => {
-                const randomIndex = Math.floor(Math.random() * data.data.length);
-                setQuote(data.data[randomIndex].quoteText);
-            })
-            .catch(error => {
-                console.error('Error fetching quote:', error);
-            });
-    }, []);
-    
+        console.log('useEffect called');
+        const timer = setTimeout(() => {
+            fetch('https://quote-garden.onrender.com/api/v3/quotes?genre=motivational')
+                .then(response => response.json())
+                .then(data => {
+                    const randomIndex = Math.floor(Math.random() * data.data.length);
+                    setQuote(data.data[randomIndex].quoteText);
+                })
+                .catch(error => {
+                    console.error('Error fetching quote:', error);
+                });
+        }, 500);
 
-    return(
+        return () => clearTimeout(timer); // Cleanup the timer if the component unmounts before the delay completes
+    }, []);
+
+    return (
         <Box>
             <Paper sx={{ minWidth: '100%', minHeight: '220px', borderBottomLeftRadius: '6%', borderBottomRightRadius: '6%', backgroundColor: '#3d71b8' }}>
-                <Typography variant='h4' sx={{ pt: 5, ml: 3, maxWidth: '80%', color: 'white' }}>{quote || 'Loading quote...'}</Typography>
+                <Typography variant='h4' sx={{ pt: 5, ml: 3, maxWidth: '80%', color: 'white' }}>{quote || 'Generating Zen, These things take time...'}</Typography>
             </Paper>
         </Box>
     );
