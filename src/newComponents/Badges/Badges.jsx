e0595e666ce7234ad5788ecfe801b62158827e5a
Mitchell Ludkey <mludk92@hotmail.com>
2023-07-13 10:50:35 -0500

    badges progess modal created

--- src/newComponents/Badges/Badges.jsx
+++ src/newComponents/Badges/Badges.jsx
@@ -1,35 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Badges.css";
-import CircularProgress from "@mui/material/CircularProgress";
+
+import ProgressModal from "./ProgressModal";
 
 function Badges() {
   const dispatch = useDispatch();
   const badgesEarned = useSelector((store) => store.badges);
   const logindata = useSelector((store) => store.logindata);
-  const userId = useSelector(state => state.user.id)
+  const userId = useSelector((state) => state.user.id);
   const [filter, setFilter] = useState("all"); // Filter state
   const [showTooltip, setShowTooltip] = useState(false);
-  const [quote, setQuote] = useState(''); // State to hold fetched quote
+  const [quote, setQuote] = useState(""); // State to hold fetched quote
+  const [showModal, setShowModal] = useState(true); // State to control modal visibility
 
   useEffect(() => {
     dispatch({ type: "FETCH_BADGES" });
     dispatch({ type: "FETCH_LOGIN_DATA" });
 
     // Fetch motivational quote from API
-    fetch('https://quote-garden.onrender.com/api/v3/quotes?genre=motivational')
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://quote-garden.onrender.com/api/v3/quotes?genre=motivational")
+      .then((response) => response.json())
+      .then((data) => {
         const randomIndex = Math.floor(Math.random() * data.data.length);
         setQuote(data.data[randomIndex].quoteText);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, [dispatch]);
 
   const handleScroll = () => {
     const container = document.querySelector(".badgescontainer");
     const indicator = document.querySelector(".indicator");
-  
+
     if (container && indicator) {
       if (container.scrollTop > 0) {
         indicator.classList.add("transparent");
@@ -38,7 +40,6 @@ function Badges() {
       }
     }
   };
-  
 
   useEffect(() => {
     const container = document.querySelector(".badgescontainer");
@@ -61,12 +62,13 @@ function Badges() {
 
   const progressLabels = [
     "",
-    `${quote}<br/>Great job! You have logged in for ${logindata[0].streak} day in a row!`,
-    `${quote}<br/>${logindata[0].streak} days now! Keep it up!`,
-    `${quote}<br/>Amazing job! You have logged in for ${logindata[0].streak} days in a row!`,
-    `${quote}<br/> What an achievement! You have logged in for ${logindata[0].streak} days in a row!`,
-    `${quote}<br/> The Progress bar is filled, just like your heart!<br/> You have logged in for ${logindata[0].streak} days in a row!`,
+    `${quote}<br/>Great job! You have logged in for <span style="font-weight:bold; font-size:25px;">${logindata[0].streak}</span> day in a row!`,
+    `${quote}<br/><span style="font-weight:bold; font-size:25px;">${logindata[0].streak}</span> days now! Keep it up!`,
+    `${quote}<br/>Amazing job! You have logged in for <span style="font-weight:bold; font-size:25px;">${logindata[0].streak}</span> days in a row!`,
+    `${quote}<br/> What an achievement! You have logged in for <span style="font-weight:bold; font-size:25px;">${logindata[0].streak}</span> days in a row!`,
+    `${quote}<br/> The Progress bar is filled, just like your heart!<br/> You have logged in for <span style="font-weight:bold; font-size:25px;">${logindata[0].streak}</span> days in a row!`,
   ];
+  
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
@@ -84,15 +86,27 @@ function Badges() {
     } else if (filter === "achieved") {
       return badge.user_id !== null && badge.user_id === userId;
     } else if (filter === "not-achieved") {
-      return badge.user_id === null ;
+      return badge.user_id === null;
     }
     return false;
   });
   console.log("filteredBadges:", filteredBadges);
-  console.log(userId, "userId")
-  console.log(badgesEarned, "badgesEarned")
+  console.log(userId, "userId");
+  console.log(badgesEarned, "badgesEarned");
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="main-container">
+      {showModal && (
+        <ProgressModal
+          onClose={closeModal}
+          progressLevel={progressLevel}
+          progressLabels={progressLabels}
+        />
+      )}
       <div className="container1">
         <div className="header1">
           <h1 className="badges-title">Badges</h1>
@@ -116,7 +130,8 @@ function Badges() {
 
         <div className="badgescontainer">
           {filteredBadges.map((badge) => {
-            const isCurrentUserBadge = badge.user_id !== null && badge.user_id === userId;
+            const isCurrentUserBadge =
+              badge.user_id !== null && badge.user_id === userId;
             const badgeImageSrc = isCurrentUserBadge
               ? `/badges/${badge.badge_name.replace(/\s/g, "")}.png`
               : `/badges/${badge.badge_name.replace(/\s/g, "")}Gray.png`;
@@ -136,37 +151,8 @@ function Badges() {
             );
           })}
           {/* Indicator */}
-          {filteredBadges.length > 4 && (
-            <div className="indicator">Scroll for more badges</div>
-          )}
+          {filteredBadges.length > 4 && <div className="indicator">Scroll for more badges</div>}
         </div>
-
-        {/* Progress Bar */}
-        <div className="progress-container">
-          <CircularProgress
-            variant="determinate"
-            value={progressLevel}
-            size={200}
-            thickness={4}
-            style={{
-              width: "100px",
-              height: "100px",
-              borderRadius: "100%",
-              color: "#83C55F",
-              borderColor: "#3D71B8",
-              borderStyle: "solid",
-              borderWidth: "6px",
-              backgroundColor: "transparent",
-              boxShadow: "0 0 20px 5px rgba(255, 255, 0, 0.5), 0 0 10px 0 rgba(255, 255, 0, 0.5)",
-            }}
-            className="progress-bar"
-          />
-        </div>
-        <div
-          className="progress-label"
-          dangerouslySetInnerHTML={{ __html: progressLabels[Math.floor(progressLevel / 20)] }}
-        ></div>
-        {showTooltip && <div className="tooltip">Tooltip Text</div>}
       </div>
     </div>
   );
